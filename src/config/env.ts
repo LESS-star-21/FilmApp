0fa30714a3b0cb990075484e1e34074d6dedb801
LESS-star-21 <leslygomez1921@gmail.com>
2026-04-15 12:48:30 -0500

    update: configuración de variables de entorno en env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-    PORT: Number(process.env.PORT) || 3001,
-    MONGOURI: process.env.MONGO_URI || 'mongodb://datab:111base@ac-6efnrx7-shard-00-00.u3xqmqa.mongodb.net:27017,ac-6efnrx7-shard-00-01.u3xqmqa.mongodb.net:27017,ac-6efnrx7-shard-00-02.u3xqmqa.mongodb.net:27017/?ssl=true&replicaSet=atlas-fnpmdz-shard-0&authSource=admin&appName=Cluster0',
-    JWTSECRET: process.env.JWT_SECRET || 'secret_film_app',
-    JWT_EXPIRATION: process.env.JWT_EXPIRATION || '10h'
+    port: Number(process.env.PORT) || 3001,
+    mongoUri: process.env.MONGO_URI || '',
+    jwtSecret: process.env.JWT_SECRET || 'secret_film_app',
+    jwtExpiration: process.env.JWT_EXPIRATION || '10h'
 };
